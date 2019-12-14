6239858cadd3a02871bef09b721232ee63f49b2c
Peyman Najafi <peynajber@gmail.com>
2019-12-14 10:30:52 +0330

    Fix names with dot

--- mconv.py
+++ mconv.py
@@ -4,47 +4,51 @@ from mutagen.mp3 import MP3
 
 conv = list(" ()|[]")
 
+
 def change_audio_rate(dir_path, speed_rate=2):
     prefix = '{}'.format(speed_rate)
     if os.path.exists(dir_path):
         folder = dir_path.split('/')[-1]
-        new_dir_path = '/'.join(dir_path.split('/')[:-1]) + '/' + prefix + '_r_' + folder
+        new_dir_path = '/'.join(dir_path.split('/')[:-1]) + '/' + prefix + '.' + folder
         os.mkdir(new_dir_path)
         l = list(os.listdir(dir_path))
         l.sort()
         for f in l: # f -> file
             fpath = dir_path + '/' + f
             fname = '_'.join(f.split('.')[:-1])
-            new_fpath = new_dir_path + '/' + fname + '_r_' + prefix + '.mp3'
+            new_fpath = new_dir_path + '/' + fname + '.' + prefix + '.mp3'
             '''
             if not os.path.exists(new_fpath):
                 open(new_fpath, 'a').close()
             '''
             for c in conv:
-                fpath = fpath.replace(c, "\{}".format(c))
-                new_fpath = new_fpath.replace(c, "\{}".format(c))
+                fpath = fpath.replace(c, f"\{c}")
+                new_fpath = new_fpath.replace(c, f"\{c}")
 
             comm = 'ffmpeg -i {old_file} -filter:a "atempo={rate}" -vn {new_file} -y'.format(
                 old_file=fpath,
                 rate=speed_rate,
                 new_file=new_fpath,
             )
-            print('\n=======================\n',comm,'\n==========================\n')
+            print('\n=======================\n', comm, '\n==========================\n')
             os.system(comm)
             print('# {} -> finished!'.format(new_fpath))
 
 
 def video_to_audio_and_change_rate(dir_path, speed_rate=2):
-    prefix = '_r_{}_r_{}'.format('ToA', speed_rate)
+    prefix = '.{}.r{}'.format('ToAud', speed_rate)
     if os.path.exists(dir_path):
         folder = dir_path.split('/')[-1]
         new_dir_path = '/'.join(dir_path.split('/')[:-1]) + '/' + folder + prefix
-        os.mkdir(new_dir_path)
+        try:
+            os.mkdir(new_dir_path)
+        except Exception as ex:
+            print('### Exception in mkdir path: ', ex)
         l = list(os.listdir(dir_path))
         l.sort()
-        for f in l: # f -> file
+        for f in l:  # f -> file
             fpath = dir_path + '/' + f
-            new_fpath = new_dir_path + '/' + f.split('.')[0] + prefix + '.mp3'
+            new_fpath = new_dir_path + '/' + '.'.join(f.split('.')[:-1]) + prefix + '.mp3'
             '''
             if not os.path.exists(new_fpath):
                 open(new_fpath, 'a').close()
@@ -64,16 +68,16 @@ def video_to_audio_and_change_rate(dir_path, speed_rate=2):
 
 
 def video_to_audio(dir_path):
-    prefix = '{}_r_'.format('ToAudio')
+    prefix = '.{}'.format('ToAud')
     if os.path.exists(dir_path):
         folder = dir_path.split('/')[-1]
         new_dir_path = '/'.join(dir_path.split('/')[:-1]) + '/' + prefix + folder
         os.mkdir(new_dir_path)
         l = list(os.listdir(dir_path))
         l.sort()
-        for f in l: # f -> file
+        for f in l:  # f -> file
             fpath = dir_path + '/' + f
-            new_fpath = new_dir_path + '/' + prefix + f.split('.')[0] + '.mp3'
+            new_fpath = new_dir_path + '/' + prefix + '.'.join(f.split('.')[:-1]) + '.mp3'
             '''
             if not os.path.exists(new_fpath):
                 open(new_fpath, 'a').close()
@@ -90,6 +94,7 @@ def video_to_audio(dir_path):
             os.system(comm)
             print('# {} -> finished!'.format(new_fpath))
 
+
 def get_files(bp):
     files = []
     for d in os.listdir(bp):
@@ -99,6 +104,7 @@ def get_files(bp):
             _files = get_files(os.path.join(bp,d))
             files.extend(_files)
     return files
+
 
 def get_audio_details(p):
     l = get_files(p)
